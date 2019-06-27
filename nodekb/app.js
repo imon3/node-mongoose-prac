70a2ddde24463fe270f9c0959d0ce22b738295b9
Imon Ovbude <imonovbude@gmail.com>
2019-06-26 17:57:18 -0700

    created views folder and layouts

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,11 +1,51 @@
+const path = require("path");
 const express = require("express");
 
 const app = express();
 
+// Load View Engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
 // Get Route Home
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    res.send("Hello World");
+    let articles = [
+      {
+        id: 1,
+        title: "Article One",
+        author: "Petter Popper",
+        body: "This is Article One"
+      },
+      {
+        id: 2,
+        title: "Article Two",
+        author: "Mike Jones",
+        body: "This is Article Two"
+      },
+      {
+        id: 3,
+        title: "Article Three",
+        author: "Tom Jerry",
+        body: "This is Article Three"
+      }
+    ];
+
+    res.render("index", {
+      title: "Hello",
+      articles: articles
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Add Route
+app.get("/articles/add", async (req, res) => {
+  try {
+    res.render("add_article", {
+      title: "Add Article"
+    });
   } catch (err) {
     console.log(err);
   }
