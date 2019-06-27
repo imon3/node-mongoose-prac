19307578000b909084dd33c63ddcb35e41f59540
Imon Ovbude <imonovbude@gmail.com>
2019-06-26 17:25:32 -0700

    added pug dependency

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+
+// Get Route Home
+app.get("/", (req, res) => {
+  try {
+    res.send("Hello World");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
